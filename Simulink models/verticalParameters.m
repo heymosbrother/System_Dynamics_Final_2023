ee73f67abf595c4cd3e9d119ec8e700941a7a27f
Heymosbrother <danielshencool@gmail.com>
2023-11-30 02:46:54 +0800

    first commit

---
+++ Simulink models/verticalParameters.m
@@ -0,0 +1,12 @@
+clc; clear; 
+% Final Project Parameters
+
+%% Only Verticle Suspension
+% Main Body
+Ms = 5; ls1 = 0.8; ls2 = 0.8; Jms = 0.5;
+
+% Linear Vibration Absorbers
+%%% Front
+Mu1 = 1; ku1 = 1; cu1 = 1; ko1 = 1;
+%%% Rear
+Mu2 = 1; ku2 = 1; cu2 = 1; ko2 = 1;
